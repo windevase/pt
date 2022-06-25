3a8064e577b0e49c0d9ae36728199ee33a1f7176
rhghwls0716 <rhghwls0716@hanmail.net>
2022-06-25 23:59:15 +0900

    first commit

--- 2. vpc.tf
+++ 2. vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-northeast-2"
-  # access_key = "AKIATPYPWNE3P7C7Z6HN"
-  # secret_key = "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"
+  access_key = "AKIATPYPWNE3P7C7Z6HN"
+  secret_key = "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"
   }
 
 resource "aws_vpc" "hjko_vpc" {    
