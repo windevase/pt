d7b7665511fc8192f80a04e17581c0a208eb0580
rhghwls0716 <rhghwls0716@hanmail.net>
2022-07-07 00:41:45 +0900

    main

--- pt/14. atsg.tf
+++ pt/14. atsg.tf
@@ -15,6 +15,42 @@ resource "aws_autoscaling_group" "atsg" {
   vpc_zone_identifier = [aws_subnet.web_subnet[1].id]
 }
 
+resource "aws_autoscalingplans_scaling_plan" "asp" {
+  name = "${format("%s-predictive-cost-SSoptimization", var.name)}"
+
+  application_source {
+    tag_filter {
+      key    = "application"
+      values = ["asp"]
+    }
+  }
+
+  scaling_instruction {
+    disable_dynamic_scaling = true
+
+    max_capacity       = 10
+    min_capacity       = 2
+    resource_id        = format("autoScalingGroup/%s", aws_autoscaling_group.atsg.name)
+    scalable_dimension = "autoscaling:autoScalingGroup:DesiredCapacity"
+    service_namespace  = "autoscaling"
+
+    target_tracking_configuration {
+      predefined_scaling_metric_specification {
+        predefined_scaling_metric_type = "ASGAverageCPUUtilization"
+      }
+
+      target_value = 50
+    }
+
+    predictive_scaling_max_capacity_behavior = "SetForecastCapacityToMaxCapacity"
+    predictive_scaling_mode                  = "ForecastAndScale"
+
+    predefined_load_metric_specification {
+      predefined_load_metric_type = "ASGTotalCPUUtilization"
+    }
+  }
+}
+
 resource "aws_autoscaling_attachment" "asatt" {
   autoscaling_group_name = aws_autoscaling_group.atsg.id
   lb_target_group_arn = aws_lb_target_group.alb_target.arn
