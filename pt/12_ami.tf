26aa1cf57c3ecea9f5a07aac70ee9a57d44b21dc
mhan <cmhh0808@naver.com>
2022-07-22 04:03:49 +0900

    ami_fix-1

--- pt/12_ami.tf
+++ pt/12_ami.tf
@@ -2,6 +2,6 @@ resource "aws_ami_from_instance" "web_ami" {
     name = "web-image"
     source_instance_id = aws_instance.web[0].id
     depends_on = [
-    aws_instance.web
+    aws_instance.web, aws_instance.ansible
   ]
 }
